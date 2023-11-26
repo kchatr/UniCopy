056cd9459bb550e709b0336b3ffe4d463bca7c53
Aayaan <66046533+AayaanN@users.noreply.github.com>
2023-11-26 17:41:22 -0500

    added loading

--- react_frontend/react-frontend/src/App.js
+++ react_frontend/react-frontend/src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
   const [inputText, setInputText] = useState('');
   const [outputText, setOutputText] = useState('');
+  const [loader, setLoader] = useState(false);
+
 
   const handleSubmit = async () => {
+    setLoader(true)
     try {
       const response = await fetch('/get_response', {
         method: 'POST',
@@ -17,6 +21,8 @@ function App() {
         body: JSON.stringify({ text: inputText }),
       });
 
+      setLoader(false);
+
       const result = await response.json();
       setOutputText(result.result);
     } catch (error) {
@@ -25,7 +31,25 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" >
+
+    {/* <Box
+      component="img"
+      sx={{
+        height: 333,
+        width: 450,
+        maxHeight: { xs: 233, md: 167 },
+        maxWidth: { xs: 350, md: 250 },
+      }}
+      pl={150}
+      alt="The house from the offer."
+      src="https://png.pngtree.com/png-clipart/20230928/original/pngtree-colorful-bulb-vector-illustration-on-white-background-efficiency-electricity-invention-vector-png-image_12896701.png"
+    /> */}
+
+      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+
+      <dotlottie-player src="https://lottie.host/4868aabb-1686-4e74-930e-3cee25f2aad1/lb12YdzQBS.json"  speed="1" width={300} height={300} loop autoplay></dotlottie-player>
+            
       <div className='p-20'>
 
         <Box pl={10} fontSize={30}>
@@ -54,9 +78,9 @@ function App() {
         
     }} onClick={handleSubmit}>Submit</Button>
         </Box>
-        
+    
         <Box className='p-20 w-full' p={10}>
-          <TextField 
+        {loader ? <CircularProgress /> : <TextField 
             id="outlined-multiline-flexible"
             label="Other Similar Patents"
             multiline
@@ -65,7 +89,18 @@ function App() {
             value={outputText}
             readOnly
             sx={{ width: 800 }}
-          />
+          />}
+        {/* <CircularProgress></CircularProgress> */}
+          {/* <TextField 
+            id="outlined-multiline-flexible"
+            label="Other Similar Patents"
+            multiline
+            maxRows={8}
+            className='w-full'
+            value={outputText}
+            readOnly
+            sx={{ width: 800 }}
+          /> */}
         </Box>
       </div>
     </div>
@@ -73,3 +108,5 @@ function App() {
 }
 
 export default App;
+
+
